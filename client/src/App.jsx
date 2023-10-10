c6e471163b3871f87afc4802a21d4efc78202bcb
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-10-09 20:11:30 -0500

    feat(client): implement basic router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,35 @@
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import BranchesPage from './pages/BranchesPage';
+import BranchProfilePage from './pages/BranchProfilePage';
+import ProductsPage from './pages/ProductsPage';
+import UserProfilePage from './pages/UserProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Bienvenido a Stockwise!</h1>
-    </>
-  )
+    <Router>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/branches" element={<BranchesPage />} />
+          <Route path="/branches/:id" element={<BranchProfilePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
-export default App
+export default App;
