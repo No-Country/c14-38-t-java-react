9a3847fdf1e976200c26f3416b8fd0a86cb4a9d2
Ricardo Huallpayunca <rickhufer@outlook.com>
2023-10-12 20:53:15 -0500

    style: Mejoras en los estilos del Login y signup

--- client/src/pages/LoginPage/index.jsx
+++ client/src/pages/LoginPage/index.jsx
@@ -4,45 +4,48 @@ const LoginPage = () => {
   return (
     <>
       <div className="flex w-full h-screen">
-        <div className="hidden px-2 sm:flex w-full gradient-main-bottom items-center justify-center flex-col">
-          <h1 className="text-center font-['Poppins'] text-5xl font-bold text-white mt-3">
+        <div className="hidden px-2 sm:flex w-full bg-blue-gradient items-center justify-center flex-col">
+          <h1 className="text-center font-['Poppins'] text-5xl font-bold text-custom-white mt-3">
             Bienvenido a Stockwise
           </h1>
-          <p className="text-2xl text-center text-white mt-4 w-80 font-medium">
+          <p className="text-2xl text-center text-custom-white mt-4 w-80 font-medium">
             Una plataforma creada para optimizar tu negocio
           </p>
           <img className="mt-4" src="/images/login-image.svg" alt="Imagen" />
         </div>
 
         <div className="flex w-full items-center justify-center">
-          <div className="w-full max-w-sm bg-white px-10 py-10 rounded shadow-md">
-            <h1 className="text-3xl font-normal">Inicia Sesión</h1>
+          <div className="w-full max-w-sm m-4 sm:bg-[white] px-10 py-10 rounded sm:shadow-md">
+            <h1 className="flex sm:hidden text-3xl bg-clip-text text-transparent bg-blue-gradient font-bold mt-2">
+              Stockwise
+            </h1>
+            <h1 className="sm:text-3xl text-lg font-normal">Inicia Sesión</h1>
             <p className="text-xs mt-2 mb-3">
               ¿Eres un usuario nuevo?{' '}
-              <span className="text-[#3E43C7] text-left mb-2 cursor-pointer underline">
-                <Link to='/signup'>Crear una cuenta</Link>
+              <span className="text-custom-blue text-left mb-2 cursor-pointer underline">
+                <Link to="/signup">Crear una cuenta</Link>
               </span>
             </p>
             <div>
-              <div className="mt-2.5 mb-2.5 flex rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <div className="mt-2.5 mb-2.5 flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md">
                 <input
                   type="email"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6"
                   placeholder="usuario@mail.com"
                 />
               </div>
-              <div className="mt-2.5 mb-1 flex rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <div className="mt-2.5 mb-1 flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md">
                 <input
                   type="password"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6"
                   placeholder="Contraseña"
                 />
               </div>
             </div>
-            <p className="mb-2.5 text-xs text-right text-[#5D5D5D] cursor-pointer underline">
+            <p className="mb-2.5 text-xs text-right text-custom-dark-gray cursor-pointer underline">
               ¿Has olvidado tu contraseña?
             </p>
-            <button className="mt-3 w-full gradient-main-bottom rounded-full text-white text-sm py-2 px-4 font-normal">
+            <button className="mt-3 w-full sm:bg-blue-gradient bg-none rounded-full sm:text-custom-white text-sm py-2 px-4 font-normal border border-custom-blue text-custom-blue">
               Ingresar
             </button>
           </div>
