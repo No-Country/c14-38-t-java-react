b894ab1dab080d27007de4359610104e2f6e3478
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-10-24 21:16:07 -0500

    feat: move Sidebar to LayoutUser

--- client/src/layout/LayoutUser.jsx
+++ client/src/layout/LayoutUser.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
-// import Sidebar from '../components/Sidebar';
+import Sidebar from '../components/Sidebar';
 
 function LayoutUser() {
   return (
     <div className='flex flex-col h-screen text-custom-blue'>
       <Header />
       <div className='flex flex-row h-full w-full'>
-        {/* <Sidebar /> */}
-        <Outlet />
+        <Sidebar />
+        <main className='flex-grow w-3/4 bg-custom-panel p-4 m-2.5 rounded-lg'>
+          <Outlet />
+        </main>
       </div>
     </div>
   );
--- client/src/pages/ProductsPage/index.jsx
+++ client/src/pages/ProductsPage/index.jsx
@@ -2,42 +2,38 @@ import { Link } from 'react-router-dom';
 import { ArrowRight } from 'react-feather';
 // import branchesData from '../../data/branchesData';
 import productsData from '../../data/productsData';
-import Sidebar from '../../components/Sidebar';
 
 const ProductsPage = () => {
   return (
     <>
-      <Sidebar />
-      <main className='flex-grow w-3/4 bg-custom-panel p-4 m-2.5 rounded-lg'>
-        <div className='flex flex-row justify-between text-custom-black'>
-          <h2>Inventario</h2>
-          <h2>Digital Express</h2>
-        </div>
-        <hr className='border-t-1 border-[#898AA3] mb-3' />
+      <div className='flex flex-row justify-between text-custom-black'>
+        <h2>Inventario</h2>
+        <h2>Digital Express</h2>
+      </div>
+      <hr className='border-t-1 border-[#898AA3] mb-3' />
 
-        <div className='text-custom-white mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 xl:grid-cols-3 xl:gap-x-8'>
-          {productsData.map((branch) => (
-            <div key={branch.id} className='group flex bg-blue-card rounded'>
-              <div
-                key={branch.id}
-                className='p-3 aspect-h-1 aspect-w-1 w-full overflow-hidden lg:aspect-none h-24 sm:h-28 lg:h-32 '
-              >
-                <h1>{branch.name}</h1>
-                <p>1200</p>
-                <h2>Productos</h2>
-              </div>
-              <div className='p-4 items-end flex'>
-                <Link
-                  to='/branches/norte'
-                  className='flex items-center justify-center bg-blue-gradient text-white rounded w-9 h-9'
-                >
-                  <ArrowRight />
-                </Link>
-              </div>
+      <div className='text-custom-white mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 xl:grid-cols-3 xl:gap-x-8'>
+        {productsData.map((branch) => (
+          <div key={branch.id} className='group flex bg-blue-card rounded'>
+            <div
+              key={branch.id}
+              className='p-3 aspect-h-1 aspect-w-1 w-full overflow-hidden lg:aspect-none h-24 sm:h-28 lg:h-32 '
+            >
+              <h1>{branch.name}</h1>
+              <p>1200</p>
+              <h2>Productos</h2>
             </div>
-          ))}
-        </div>
-      </main>
+            <div className='p-4 items-end flex'>
+              <Link
+                to='/branches/norte'
+                className='flex items-center justify-center bg-blue-gradient text-white rounded w-9 h-9'
+              >
+                <ArrowRight />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
