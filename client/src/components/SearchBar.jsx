9e7e9686bb89d85d8f9dea112250ba06621d21e3
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-10-24 23:25:08 -0500

    feat: add SearchBar component

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,27 @@
+import { SearchIcon } from './Icons';
+import { cn } from '../utils/cn';
+
+export const SearchBar = ({ className, children, ...props }) => {
+  return (
+    <div
+      className={cn(
+        'flex items-center rounded-full bg-[#E5E5E5] px-2.5 border border-[#C8C8C8] text-custom-dark-gray',
+        className,
+      )}
+    >
+      <label htmlFor='search' className='pl-2'>
+        <SearchIcon />
+      </label>
+
+      <input
+        id='search'
+        name='search'
+        type='search'
+        className='flex-grow border-0 ring-0 focus-visible:border-0 focus-visible:ring-0 bg-transparent'
+        {...props}
+      />
+
+      {children}
+    </div>
+  );
+};
