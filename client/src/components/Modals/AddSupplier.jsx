587224290b576dfe657a9224c0a8527e912f80d7
Edwin <carmonaedwin1123@gmail.com>
2023-10-25 21:51:22 -0500

    Añadiendo la parte para seleccionar categoría y proveedor simulando la selección con un array para cada uno

--- client/src/components/Modals/AddSupplier.jsx
+++ client/src/components/Modals/AddSupplier.jsx
@@ -1,35 +1,63 @@
-const AddSupplier = ({isSupplierModal, setIsSupplierModal}) => {
+import { useState } from "react"
+import { Search, Check } from "react-feather"
+
+const supplier = [
+    {id: 1, name: 'MicroGlobal'}
+]
+
+const AddSupplier = ({isSupplierModal, setIsSupplierModal, setSupplier}) => {
+    const [selectSupplier, isSelectSupplier] = useState(false)
+    const [selectedSupplier, setSelectedSupplier] = useState("");
+
+    const handleItemClick = (supplierName) => {
+        setSelectedSupplier(supplierName);
+    };
+
+    const handleAddClick = () => {
+        setSupplier(selectedSupplier);
+        setIsSupplierModal(false);
+    };
     return (
         <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-            <div className="bg-gray-100 sm:bg-white p-4 sm:pt-9 sm:pb-8 sm:rounded-xl flex flex-col sm:justify-center sm:align-baseline gap-5 relative w-full sm:w-auto h-full sm:h-auto overflow-auto sm:overflow-visible">
+            <div className="bg-gray-100 sm:bg-white p-4 sm:pt-9 sm:pb-8 sm:rounded-xl flex flex-col sm:justify-center sm:align-baseline gap-5 absolute sm:relative w-full sm:w-auto h-full sm:h-auto overflow-auto sm:overflow-visible">
                 <button className="absolute sm:top-0 sm:right-0 sm:my-4 mt-0.5 ml-3 sm:mx-8" onClick={() => setIsSupplierModal(false)}>
-                    <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="w-6 h-6 hidden sm:block">
+                    <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="w-6 h-6 hidden sm:block text-custom-black">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
                     </svg>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 sm:hidden">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                     </svg>
                 </button>
-                <h3 className="flex font-[Roboto] text-xl font-medium items-start mx-12 sm:mx-16">Proveedores</h3>
+                <h3 className="flex font-[Roboto] text-xl font-medium items-start mx-12 sm:mx-16 text-custom-black">Proveedores</h3>
                 <div className="flex flex-col gap-5 mx-4 sm:ml-0 sm:mr-0">
                     <div className="flex items-center bg-gray-200 rounded-3xl shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:w-auto lg:w-96 lg:h-10 sm:ml-14 sm:mr-16">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-4">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                        </svg>
+                        <Search className="w-4 h-4 ml-4 text-custom-black"/>
                         <input type="text" className="block flex-1 border-0 bg-transparent py-2.5 pl-3 pr-6 text-custom-black placeholder:text-custom-black focus:ring-0 sm:text-sm sm:leading-6" placeholder="Buscar proveedor" />
                     </div>
-                    <button className="flex justify-center sm:justify-start items-center fixed bottom-10 right-4 sm:static sm:bg-gray-200 bg-custom-blue text-custom-white sm:text-custom-black sm:py-2.5 sm:pl-3 sm:pr-6 w-14 h-14 sm:w-auto lg:w-96 lg:h-10 sm:ml-14 sm:mr-16 border border-solid border-custom-gray sm:rounded rounded-full z-10">
-                        <span className="hidden sm:flex ml-5 text-sm">+ Agrega un proveedor nuevo</span>
-                        <span className="flex sm:hidden text-2xl">+</span>
-                    </button>
-                    <div className="w-full h-screen sm:h-36 sm:ml-14 sm:mr-16 overflow-auto sm:overflow-visible"></div>
+                    <button className="sm:flex hidden justify-start items-center bg-gray-200  text-custom-black py-2.5 pl-3 pr-6 w-auto lg:w-96 lg:h-10 ml-14 mr-16 border border-solid border-custom-gray rounded" onClick={() => isSelectSupplier(true)}>
+                      <span className="flex ml-5 text-sm">+ Agrega un proveedor nuevo</span>
+                  </button>
+                  <div className="h-screen sm:h-36 sm:ml-14 sm:mr-16 overflow-auto">
+                    <ul className="flex flex-col gap-0.5 bg-white">
+                        {selectSupplier && (
+                                supplier.map((item) => (
+                                    <div key={item.id} className="flex flex-col gap-0.5">
+                                        <li key={item.id} className={`bg-gray-200 text-custom-black py-2.5 px-8 cursor-pointer border border-solid  ${selectedSupplier === item.name ? 'border-custom-blue' : ''}`} onClick={() => handleItemClick(item.name)}>{item.name}</li>
+                                        {selectedSupplier === item.name && ( <span className="text-custom-blue text-xs">Podrás agregar más información en la sección de "Proveedores"</span>)}
+                                    </div>
+                                    )
+                                )
+                        )}
+                   </ul>
+                  </div>
                     <div className="flex justify-end gap-8 mx-3 sm:mx-8 sm:mt-auto">
                         <button className="hidden sm:flex items-center justify-center w-24 h-10 py-2.5 text-custom-blue border border-custom-blue rounded-3xl text-sm" onClick={() => setIsSupplierModal(false)}>Cancelar</button>
-                        <button className="flex items-center justify-center w-8 h-8 sm:w-24 sm:h-10 absolute sm:static top-4 text-custom-blue sm:text-custom-white sm:bg-blue-gradient sm:text-sm sm:border sm:border-custom-blue rounded-3xl">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 sm:hidden">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                            </svg>
+                        <button className="flex items-center justify-center w-8 h-8 sm:w-24 sm:h-10 absolute sm:static top-4 text-custom-blue sm:text-custom-white sm:bg-blue-gradient sm:text-sm sm:border sm:border-custom-blue rounded-3xl" onClick={handleAddClick}>
+                            <Check className="w-8 h-8 sm:hidden"/>
                             <span className="hidden sm:block">Agregar</span>
+                        </button>
+                        <button className="flex sm:hidden justify-center items-center fixed bottom-10 right-4 bg-custom-blue text-custom-white w-14 h-14 rounded-full z-10">
+                            <span className="flex sm:hidden text-2xl">+</span>
                         </button>
                     </div>
                 </div>
--- client/src/pages/AddProduct/index.jsx
+++ client/src/pages/AddProduct/index.jsx
@@ -1,52 +1,56 @@
 import { useState } from "react"
 import AddCategory from "../../components/Modals/AddCategory"
 import AddSupplier from "../../components/Modals/AddSupplier"
+import { Check } from "react-feather"
 
 export const AddProduct = () => {
 
     const [isCategoryModal, setIsCategoryModal] = useState(false);
     const [isSupplierModal, setIsSupplierModal] = useState(false);
 
+    const [category, setCategory] = useState("");
+    const [supplier, setSupplier] = useState("");
+
     return (
         <>
-          <div className=" bg-gray-100 sm:bg-white p-8 sm:pt-9 sm:pb-12 sm:rounded-xl flex flex-col sm:justify-center sm:align-baseline gap-5 absolute top-0 left-0 bottom-0 right-0 sm:relative w-full sm:w-auto h-full sm:h-auto overflow-auto sm:overflow-visible">
-              <button className="absolute sm:top-0 sm:right-0 sm:my-4 mt-0.5 ml-3 sm:mx-8">
+          <div className=" bg-gray-100 sm:bg-white sm:rounded-xl flex flex-col sm:justify-center sm:align-baseline gap-4 absolute top-0 left-0 bottom-0 right-0 sm:static w-full h-auto sm:w-auto sm:h-auto overflow-auto sm:overflow-visible">
+              <button className="absolute top-8 sm:static ml-3 sm:mx-8 text-custom-black">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 sm:hidden">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                   </svg>
               </button>
-              <h3 className="hidden sm:flex font-[Roboto] text-xl font-medium items-start mx-12 sm:mx-0 sm:mb-8">Agregar Ítem al inventario</h3>
-              <h3 className="sm:hidden font-[Roboto] text-xl font-medium items-start mx-12 sm:mx-16">Agregar Ítem</h3>
-              <div className="flex flex-col-reverse gap-3 sm:gap-32 mx-4 sm:mx-auto sm:flex-row">
-                  <div className="flex flex-col sm:w-1/2">
-                      <div className='mb-1.5 sm:mb-3 font-sans font-medium text-base'>Nombre</div>
-                      <div className="mb-3 sm:mb-3 flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen">
+              <h3 className="hidden sm:flex font-[Roboto] text-xl font-medium items-start mx-12 sm:mx-8 sm:mb-0 text-custom-black">Agregar Ítem al inventario</h3>
+              <h3 className="sm:hidden font-[Roboto] text-xl font-medium items-start mx-12 mt-8 text-custom-black">Agregar Ítem</h3>
+              <div className="flex flex-col-reverse gap-3 sm:gap-28 mx-4 sm:mx-auto sm:flex-row">
+                  <div className="flex flex-col sm:w-1/2 gap-2 sm:gap-0.5">
+                      <div className=' font-sans font-medium text-base text-custom-black'>Nombre</div>
+                      <div className=" flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen">
                           <input type="text" className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6" placeholder="Nombre del producto" />
                       </div>
-                      <div className='mb-1.5 sm:mb-3 font-sans font-medium text-base'>Código</div>
-                      <div className="mb-3 sm:mb-3 flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen">
+                      <div className=' font-sans font-medium text-base text-custom-black'>Código</div>
+                      <div className=" flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen">
                           <input type="number" className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6" placeholder="Código" />
                       </div>
-                      <div className='mb-1.5 sm:mb-3 font-sans font-medium text-base'>Descripción</div>
+                      <div className=' font-sans font-medium text-base text-custom-black'>Descripción</div>
                       <div className="mb-3 sm:mb-3 flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen">
                           <textarea type="text" className="block flex-1 border-0 h-20 resize-none bg-transparent py-1.5 pl-3 text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6" placeholder="Descripción del producto" />
                       </div>
-                      <div className='mb-1.5 sm:mb-3 font-sans font-medium text-base'>Categoría</div>
-                      <div className="mb-3 sm:mb-3 flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen" onClick={() => setIsCategoryModal(true)}>
-                          <input className="block flex-1 border-0 bg-transparent py-1.5 pl-3  text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6" placeholder="Categoría" />
+                      <div className=' font-sans font-medium text-base text-custom-black'>Categoría</div>
+                      <div className=" flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen" onClick={() => setIsCategoryModal(true)}>
+                          <input className="block flex-1 border-0 bg-transparent py-1.5 pl-3  text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6" placeholder="Categoría" value={category} onChange={(e) => setCategory(e.target.value)} readOnly/>
                       </div>
-                      {isCategoryModal && (<AddCategory isCategoryModal={isCategoryModal} setIsCategoryModal={setIsCategoryModal} />)}
-                      <div className='mb-1.5 sm:mb-3 font-sans font-medium text-base'>Proveedor</div>
-                      <div className="mb-3 sm:mb-3 flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen" onClick={() => setIsSupplierModal(true)}>
-                          <input type="text" className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6" placeholder="Proveedor del producto" />
+                      {isCategoryModal && (<AddCategory isCategoryModal={isCategoryModal} setIsCategoryModal={setIsCategoryModal} setCategory={setCategory} />)}
+                      <div className=' font-sans font-medium text-base text-custom-black'>Proveedor</div>
+                      <div className=" flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen" onClick={() => setIsSupplierModal(true)}>
+                          <input type="text" className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6" placeholder="Proveedor del producto" value={supplier} onChange={(e) => setSupplier(e.target.value)} readOnly/>
                       </div>
-                      {isSupplierModal && (<AddSupplier isSupplierModal={isSupplierModal} setIsSupplierModal={setIsSupplierModal} />)}
-                      <div className='mb-1.5 sm:mb-3 font-sans font-medium text-base'>Cantidad</div>
-                      <div className="mb-3 sm:mb-3 flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen">
+                      {isSupplierModal && (<AddSupplier isSupplierModal={isSupplierModal} setIsSupplierModal={setIsSupplierModal} setSupplier={setSupplier} />)}
+                      <div className='font-sans font-medium text-base text-custom-black'>Cantidad</div>
+                      <div className=" flex rounded-lg shadow-sm ring-1 ring-inset ring-custom-gray focus-within:ring-2 focus-within:ring-inset focus-within:ring-custom-blue sm:max-w-md sm:w-auto lg:w-screen">
                           <input type="number" className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-custom-black placeholder:text-custom-gray focus:ring-0 sm:text-sm sm:leading-6" placeholder="Stock" />
                       </div>
                   </div>
-                  <div className="flex flex-col sm:justify-center lg:justify-normal sm:pt-9 sm:w-1/2">
+                  <div className="flex flex-col sm:justify-center lg:justify-normal sm:pt-6 sm:w-1/2">
                       <label htmlFor='file-img' className="cursor-pointer">
                           <img src='/images/media.png' alt="media" className="hidden sm:block rounded-xl" />
                           <img src='/images/media-responsive.png' alt="media" className="sm:hidden w-full" />
@@ -55,12 +59,10 @@ export const AddProduct = () => {
                       </label>
                   </div>
               </div>
-              <div className="flex justify-end gap-11 mx-3 mt-28 sm:mx-0 sm:mt-1">
+              <div className="flex justify-end gap-11 mx-3 mt-28 sm:mx-8 sm:my-4 sm:mt-0">
                   <button className="hidden sm:flex items-center justify-center w-36 h-11 text-custom-blue text-xs border border-custom-blue rounded-3xl">Cancelar</button>
                   <button className="flex items-center justify-center w-8 h-8 sm:w-36 sm:h-11 absolute sm:static top-8 text-custom-blue sm:text-custom-white sm:bg-blue-gradient sm:text-xs sm:border sm:border-custom-blue rounded-3xl">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 sm:hidden">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                      </svg>
+                      <Check className="w-8 h-8 sm:hidden" />
                       <span className="hidden sm:block">Agregar</span>
                   </button>
               </div>
