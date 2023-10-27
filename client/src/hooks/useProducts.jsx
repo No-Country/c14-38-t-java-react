2d53568db0d3a9d05d25e2789546388122969558
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-10-27 16:53:44 -0500

    fix: product list renders correctly after login

--- client/src/hooks/useProducts.jsx
+++ client/src/hooks/useProducts.jsx
@@ -1,21 +1,25 @@
 import axios from 'axios';
 import { createContext, useContext, useEffect, useState } from 'react';
+import useAuthContext from './useAuthContext';
 
 const ProductContext = createContext();
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const { isAuth } = useAuthContext();
 
   useEffect(() => {
-    axios
-      .get('/api/product/all')
-      .then((response) => {
-        setProducts(response.data.body);
-      })
-      .catch((error) => {
-        console.error('Error al cargar los productos:', error);
-      });
-  }, []);
+    if (isAuth) {
+      axios
+        .get('/api/product/all')
+        .then((response) => {
+          setProducts(response.data.body);
+        })
+        .catch((error) => {
+          console.error('Error al cargar los productos:', error);
+        });
+    }
+  }, [isAuth]);
 
   return (
     <ProductContext.Provider
