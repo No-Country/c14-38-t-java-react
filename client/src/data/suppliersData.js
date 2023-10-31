296ffe79864590c8f17ee12fe5645eff0843b443
Ricardo Huallpayunca <rickhufer@outlook.com>
2023-10-30 20:31:13 -0500

    feat: add Fake data

---
+++ client/src/data/suppliersData.js
@@ -0,0 +1,39 @@
+const suppliersData = [
+  {
+    id: 1,
+    name: 'Proveedor 1',
+    address: 'La Chimba 564',
+    phone: '9876567',
+    email: 'contacto@microglobal.com',
+  },
+  {
+    id: 2,
+    name: 'Proveedor 2',
+    address: 'Rio Poblado 234',
+    phone: '234543567',
+    email: 'contacto@sony.com',
+  },
+  {
+    id: 3,
+    name: 'Proveedor 3',
+    address: 'Rio Poblado 234',
+    phone: '234543567',
+    email: 'contacto@sony.com',
+  },
+  {
+    id: 4,
+    name: 'Proveedor 4',
+    address: 'Rio Poblado 234',
+    phone: '234543567',
+    email: 'contacto@sony.com',
+  },
+  {
+    id: 5,
+    name: 'Proveedor 5',
+    address: 'Rio Poblado 234',
+    phone: '234543567',
+    email: 'contacto@sony.com',
+  },
+];
+
+export default suppliersData;
